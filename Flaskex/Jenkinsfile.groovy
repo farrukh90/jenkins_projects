998eb5ce1a4e3a9ca3297254d0e4a331cb4b4b28
farrukh90 <farrukhsadykov@gmail.com>
2019-08-28 19:45:20 -0500

    added new repo

---
+++ Flaskex/Jenkinsfile.groovy
@@ -0,0 +1,26 @@
+node{
+    stage("Update jenkins"){
+        properties([parameters([string(defaultValue: '63.33.56.132', description: 'Please provide IP', name: 'ENVIR', trim: true)])])
+        sh "echo Parameter added"
+    }
+    stage("Install git"){
+        sh "ssh  ec2-user@${ENVIR} sudo yum install git python-pip -y"
+    }
+    stage("Remove repo"){
+        //sh "ssh  ec2-user@${ENVIR} sudo  rm -rf /home/ec2-user/flask-examples"
+    }
+    stage("Pull Repo"){
+        sh "ssh  ec2-user@${ENVIR} git clone https://github.com/anfederico/Flaskex.git"
+    }
+    stage("Install Requirements"){
+        //sh "virtualenv /tmp/venv"
+        //sh ". /tmp/venv/bin/activate"
+        sh "echo Hello"
+    }
+    stage("Pip Install"){
+        sh "ssh  ec2-user@${ENVIR} sudo pip install -r /home/ec2-user/Flaskex/requirements.txt"
+    }
+    stage("Run App"){
+        sh "ssh  ec2-user@${ENVIR}  python /home/ec2-user/Flaskex/app.py"
+    }
+}
